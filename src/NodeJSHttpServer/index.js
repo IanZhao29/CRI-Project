79d8c510732d23bee8215f5764b731826c79e919
IanZhao29 <Jian.Zhao.2020@mumail.ie>
2021-12-15 15:13:44 +0800

    复原2021/12/15

--- src/NodeJSHttpServer/index.js
+++ src/NodeJSHttpServer/index.js
@@ -1,6 +1,7 @@
 const amqp = require('amqplib/callback_api');
 let msgStack = [];
 let boolSwitch = "1";
+let location = null;
 let url = {
     protocol: 'amqp',
     hostname: '8.141.56.170',
@@ -37,11 +38,13 @@ amqp.connect(url, function(error0, connection) {
             queue,
             msg => {
                 msgStack = JSON.parse(msg.content.toString())
+                console.log("收到消息：", msgStack)
                 msgStack.dateTime = new Date().toLocaleTimeString()
                 msgStack.humidity = parseInt(msgStack.humidity);
                 msgStack.ppm = parseFloat(msgStack.ppm.toFixed(1));
                 msgStack.temperature = parseFloat(msgStack.temperature.toFixed(1));
-                console.log("收到消息：", msgStack)
+                msgStack.longitude = parseFloat(msgStack.longitude);
+                msgStack.latitude = parseFloat(msgStack.latitude);
                 channel.ack(msg);
             },
             opt
